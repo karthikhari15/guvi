3fdbbd8e02d11562fd080ea110de8996a57a487e
KARTHIK R <36226617+karthikhari15@users.noreply.github.com>
2018-09-24 14:51:10 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+
+import java.io.*;
+
+
+
+ class Largest 
+{
+
+   
+ public static void main(String[] args)
+ {
+
+     
+   int n1 = 1, n2 = 1, n3 = -1;
+
+       
+ if( n1 >= n2 && n1 >= n3)
+           
+ System.out.println(n1 + "");
+
+     
+  
+ else if (n2 >= n1 && n2 >= n3)
+      
+      System.out.println(n2 + "");
+
+     
+   
+else
+            
+System.out.println(n3 + "");
+ 
+   }
+
+}
