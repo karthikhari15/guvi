cd8e1dd3ff7a00385cd345166d4219bedcd4809a
KARTHIK R <36226617+karthikhari15@users.noreply.github.com>
2018-09-24 14:30:26 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+ class Alphabet {
+
+    public static void main(String[] args) {
+
+        char c = 'd';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println("Alphabet");
+        else
+            System.out.println("no");
+    }
+}
