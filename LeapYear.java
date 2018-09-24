af5edbf2a69fe86c1ef84c5d18b464a241028c71
KARTHIK R <36226617+karthikhari15@users.noreply.github.com>
2018-09-24 15:11:14 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+import java.lang.*;
+
+
+import java.io.*;
+
+ 
+class LeapYear 
+{
+
+   
+ public static void main(String[] args) 
+
+
+{
+
+        
+int year;
+     
+   Scanner in = new Scanner(System.in);
+   
+
+   
+year=in.nextInt();
+    
+    boolean leap = false;
+
+     
+   if(year % 4 == 0)
+       
+ {
+          
+  if( year % 100 == 0)
+    
+        {
+          
+      
+              
+  if ( year % 400 == 0)
+  
+                  leap = true;
+   
+             else
+                
+    leap = false;
+           
+
+ }
+          
+  else
+              
+  leap = true;
+     
+   }
+     
+   else
+   
+         leap = false;
+
+  
+      if(leap)
+           
+ System.out.println("no");
+    
+    else
+          
+  System.out.println("yes");
+    
+}
+
+}
